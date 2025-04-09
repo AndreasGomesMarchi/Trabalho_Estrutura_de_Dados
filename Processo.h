57165fe2c1fa6e357426e499db2373f4a42be851
Andreas Gomes Marchi <162516125+AndreasGomesMarchi@users.noreply.github.com>
2025-04-09 13:01:38 -0300

    Update Processo.h

--- Processo.h
+++ Processo.h
@@ -1,4 +1,4 @@
-
+//Aqui tem um cabeçalho onde tem as structs, difinir tipo, cabeçaho das informações, etc
 #ifndef PROCESSO_H
 #define PROCESSO_H
 
@@ -21,10 +21,18 @@ typedef struct Processo{
 
 int Ler_processos(const char *nome_arquivo, Processo processos[], int max_processos);
 
+//Parte artur
+void ordenar_por_id(Processo processos[], int quantidade);
+void ordenar_por_data(Processo processos[], int quantidade);
 
+//Parte Breno
+char IdClasseDesejado[20] = "12377";
+int contarPorIdClasse(Processo processos[], int n, const char *idClasse);
+int contarIdAssuntosUnicos(Processo processos[], int n);
 
-
-
+//Parte David
+void listar_processos_com_multiplos_assuntos(Processo processos[], int n);
+int calcular_dias_em_tramitacao(const char *data_ajuizamento);
 
 
 #endif
