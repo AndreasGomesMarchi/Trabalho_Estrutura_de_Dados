9dec96c6c11d37e275650bad0c24616641f017f8
Andreas Gomes Marchi <162516125+AndreasGomesMarchi@users.noreply.github.com>
2025-04-10 11:24:22 -0300

    Update Processo.h
    Tirei os comentários para ficar mais limpo essa parte.

--- Processo.h
+++ Processo.h
@@ -1,4 +1,3 @@
-//Aqui tem um cabeçalho onde tem as structs, difinir tipo, cabeçaho das informações, etc
 #ifndef PROCESSO_H
 #define PROCESSO_H
 
@@ -7,32 +6,29 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_PROCESSOS 1000
+#define MAX_ASSUNTOS 10
 
-typedef struct Processo{
+typedef struct Processo {
     int id;
     char numero[20];
-    char data_ajuizamento[50];
+    char data_ajuizamento[100];
     int id_classe;
-    int id_assunto[10];
+    int id_assunto[MAX_ASSUNTOS];
     int num_assuntos;
     int ano_eleicao;
-}Processo;
+} Processo;
 
-int Ler_processos(const char *nome_arquivo, Processo processos[], int max_processos);
-
-//Parte artur
+int ler_processos(const char *nome_arquivo, Processo **processos);
+void imprimir_processos(Processo processos[], int quantidade);
 void ordenar_por_id(Processo processos[], int quantidade);
 void ordenar_por_data(Processo processos[], int quantidade);
-
-//Parte Breno
-char IdClasseDesejado[20] = "12377";
-int contarPorIdClasse(Processo processos[], int n, const char *idClasse);
-int contarIdAssuntosUnicos(Processo processos[], int n);
-
-//Parte David
+int contar_por_id_classe(Processo processos[], int n, int id_classe);
+void imprimir_por_id_classe(Processo processos[], int n, int id_classe);
+int contar_assuntos_unicos(Processo processos[], int n);
+void imprimir_assuntos_unicos(Processo processos[], int n);
 void listar_processos_com_multiplos_assuntos(Processo processos[], int n);
 int calcular_dias_em_tramitacao(const char *data_ajuizamento);
-
+void imprimir_processo(Processo p);
+int salvar_csv(const char *nome_arquivo, Processo processos[], int quantidade, const char *cabecalho);
 
 #endif
